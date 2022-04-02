94ef7f1d202edbb6b2fa66a6b9b9d3375e0220bb
Mohsin-Hossain <mohsinshawon18@gmail.com>
2022-04-02 14:40:03 +0600

    header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='header-nav'>
+            <h4>Cricket Accessories</h4>
+        </nav>
+    );
+};
+
+export default Header;
